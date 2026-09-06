e4d97cc0b085f5ec535eb37272c5ea8d5e2f9b6e
Cláudio Slv <claudio1748@ymail.com>
2026-09-06 15:34:55 -0300

    Adicionar navegacao principal

---
+++ src/components/app-nav.tsx
@@ -0,0 +1,2 @@
+import Link from "next/link";
+export function AppNav(){return <nav className="nav"><Link href="/">Início</Link><Link href="/participantes">Pessoas</Link><Link href="/carteira">Carteira</Link><Link href="/jogos">Jogos</Link></nav>}
